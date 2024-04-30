daddf4ea53f47a286562336a79a01d0e89d5c76a
Casper Medin Jensen <casper.jensen@hotmail.se>
2024-04-30 15:48:38 +0200

    Refactor radar.lua to use CHANNEL constants for improved code readability and maintainability

--- lua/radar.lua
+++ lua/radar.lua
@@ -15,26 +15,19 @@ end
 
 -- Tick function that will be executed every logic tick
 function onTick()
-    local CHANNEL_ASSIGNMENT = {
-        distance = gn(1),
-        azimuth = gn(2),
-        elevation = gn(3),
+    local CHANNEL = {
+        dist = gn(1),
+        azi = gn(2),
+        ele = gn(3),
         radar_rotation = gn(4),
-        radar_unit_gps_location_x = gn(5),
-        radar_unit_gps_location_y = gn(6),
-        radar_unit_gps_location_z = gn(7),
+        radar_x = gn(5),
+        radar_y = gn(6),
+        radar_z = gn(7),
         radar_lock = gb(1)
     }
-    -- Check if all the variables have been assigned
-    if CHANNEL_ASSIGNMENT.distance == nil or CHANNEL_ASSIGNMENT.azimuth == nil or CHANNEL_ASSIGNMENT.elevation == nil or
-        CHANNEL_ASSIGNMENT.radar_rotation == nil or CHANNEL_ASSIGNMENT.radar_unit_gps_location_x == nil or
-        CHANNEL_ASSIGNMENT.radar_unit_gps_location_y == nil or CHANNEL_ASSIGNMENT.radar_unit_gps_location_z == nil or
-        CHANNEL_ASSIGNMENT.radar_lock == nil then
-        error("Not all variables have been assigned", 2)
-    end
     -- Check if the radar lock is true
-    if target.radar_lock then
-        sendRadarData(1, target.azimuth, target.elevation, target.distance, target.radar_rotation,
-            target.radar_unit_gps_location_x, target.radar_unit_gps_location_y, target.radar_unit_gps_location_z)
+    if CHANNEL.radar_lock then
+        sendRadarData(1, CHANNEL.azi, CHANNEL.ele, CHANNEL.dist, CHANNEL.radar_rotation, CHANNEL.radar_x,
+            CHANNEL.radar_y, CHANNEL.radar_z)
     end
 end
